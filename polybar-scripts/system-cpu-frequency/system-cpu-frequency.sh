7508e06326ff4e1b28bd8e4dc98c6ff46a82035d
tosti <andrea.tosti@live.it>
2019-09-21 22:10:54 +0200

    Move files

--- polybar-scripts/system-cpu-frequency/system-cpu-frequency.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-freq=$(cpupower frequency-info -fm | grep -oP '(?<=frequency: )([^ ]+ [^ ]+)')
-#echo " $freq "
-filler='________'
-padded="$(printf "%s" "$freq${filler:${#freq}}")"
-echo " ${padded//'_'/' '}"
--- polybar-scripts/system-cpu-temppercore/system-cpu-temppercore.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-temp=$(sensors | grep Core | awk '{print substr($3, 2, length($3)-5)}' | tr "\\n" " " | sed 's/ /°C  /g' | sed 's/  $//')
-
-echo " $temp "
